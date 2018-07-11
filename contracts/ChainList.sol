ec618f8ba86ea2ab2dfc9f6ddffae6d1846774fe
Arpit <rpittrer@gmail.com>
2018-07-11 21:08:08 +0530

    solium

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -7,6 +7,9 @@ contract ChainList {
     string description;
     uint256 price;
 
+    constructor () public {
+        sellArticle("Default article", "This is an article set by default", 100000000000);
+    }
     // sell an article
     function sellArticle(string _name, string _description, uint256 _price) public {
         seller = msg.sender;
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -8,7 +8,7 @@ contract Migrations {
         if (msg.sender == owner) _;
     }
 
-    function Migrations() public {
+    constructor () public {
         owner = msg.sender;
     }
 
@@ -18,6 +18,6 @@ contract Migrations {
 
     function upgrade(address new_address) public restricted {
         Migrations upgraded = Migrations(new_address);
-        upgraded.setCompleted(last_completed_migration);
+upgraded.setCompleted(last_completed_migration);
     }
 }
